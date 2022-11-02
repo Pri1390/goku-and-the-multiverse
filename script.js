cc57902e72897c211f1de8a4a8cf10131fc7866d
Priscila Vieira <freedom.pva@gmail.com>
2022-11-02 18:17:06 -0300

    adiciona audio na tela inicial

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const goku = document.querySelector(".goku");
 const portal = document.querySelector(".portal");
 const start = document.getElementById("start");
 const btnStart = document.querySelector(".btnStart");
+const btnVoltar = document.querySelector(".btnVoltar")
 const gameBoard = document.querySelector(".game-board");
 const modalGameOver = document.getElementById("modalgameover");
 
 
 
 
-//adiciona e remove o pulo
+//adiciona o pulo
 const jump = () => {
     goku.classList.add("jump");
 
@@ -26,6 +27,7 @@ const loop = setInterval(() =>{
 
     const portalPosition = portal.offsetLeft;
     const gokuPosition = +window.getComputedStyle(goku).bottom.replace("px","");
+    
     if (portalPosition <= 120 && portalPosition> 0 && gokuPosition < 80){
 
 // encerra a animação quando ocorre a colisão       
@@ -38,21 +40,22 @@ const loop = setInterval(() =>{
 // muda a imgem para o Ricky
         goku.src ="./images/ricky.gif";
 
-
         gameOver();
-
-        clearInterval(loop);
-       
-
-    }
-
+        clearInterval(loop);       
+   }
 }, 10);
+
 // evento de clicar e apertar tela
 document.addEventListener("keydown", jump);
 document.addEventListener("click", jump);
 
-// toca o audio
+//voltar para a tela inicial
+btnVoltar.addEventListener("click", function () {
+    location.reload();
+  });
 
+
+// toca o audio
 const backgroundSound = new Audio();
 backgroundSound.src ="./sounds/DragonballZ-Abertura.mp3";
 
